36794991a8806501499098cae9b455aeaeab8a6a
Eric Drejza <edrejza@udel.edu>
2020-10-21 00:49:23 -0400

    random_array_producer.py now produces an array of random numbers and prints it.

--- random_array_producer.py
+++ random_array_producer.py
@@ -1,11 +1,27 @@
 import sys
 import argparse
+import random
 
-parser = argparse.ArgumentParser()
-args = parser.parse_args()
+
+def randArray(size):
+  a = [random.randint(0,9999) for _ in range(size)]
+  for e in a:
+    e * 10000
+  return a
+
+# parser = argparse.ArgumentParser()
+# args = parser.parse_args()
+args = sys.argv
+print(args)
+print(len(args))
 
 if len(args) > 2:
   print("Too many arguments")
   exit
+elif len(args) == 1:
+  size = int(input("How many elements? "))
+else:
+  size = int(args[1])
 
-elif 
+array = randArray(size)
+print(array)
