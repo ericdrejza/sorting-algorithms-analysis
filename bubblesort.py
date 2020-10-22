4d17cb31c72862717fdf514394ce5d1b5f71e242
Eric Drejza <edrejza@udel.edu>
2020-10-22 00:20:28 -0400

    Bubblesort implementation is complete

---
+++ bubblesort.py
@@ -0,0 +1,21 @@
+# Python Bubblesort Implementation
+
+# sorts the array in ascending order using the bubblesort algorithm
+def bubblesort(array):
+    
+    # length of the array
+    n = len(array)
+
+    # flag to tell us if we have swapped this iteration
+    swapped = True
+
+    while swapped:
+        # set swapped to false to start this iteration
+        swapped = False
+        for i in range(n - 1):
+            # if the previous value is less that the next value, swap
+            if array[i] > array [i + 1]:
+                # swap the values at these indices
+                array[i],array[i + 1] = array[i + 1], array[i]
+                # set flag as swapped
+                swapped = True
