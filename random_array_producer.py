89468016504e311f0ea8b5be701f4d8d217d82f8
Eric Drejza <edrejza@udel.edu>
2020-10-22 00:21:45 -0400

    random_array_producer.py is protected by main test.
    Added parameters for randArray().

--- random_array_producer.py
+++ random_array_producer.py
@@ -2,26 +2,28 @@ import sys
 import argparse
 import random
 
-
-def randArray(size):
-  a = [random.randint(0,9999) for _ in range(size)]
+# Returns an array of random integer elements between the range of 0 and 9999
+# Size of the array is determined by the size arument
+def randArray(size, lowerBound, upperBound):
+  a = [random.randint(lowerBound, upperBound) for _ in range(size)]
   for e in a:
     e * 10000
   return a
 
-# parser = argparse.ArgumentParser()
-# args = parser.parse_args()
-args = sys.argv
-print(args)
-print(len(args))
+if __name__ == "__main__":
+    # parser = argparse.ArgumentParser()
+    # args = parser.parse_args()
+    args = sys.argv
+    print(args)
+    print(len(args))
 
-if len(args) > 2:
-  print("Too many arguments")
-  exit
-elif len(args) == 1:
-  size = int(input("How many elements? "))
-else:
-  size = int(args[1])
+    if len(args) > 2:
+      print("Too many arguments")
+      exit
+    elif len(args) == 1:
+      size = int(input("How many elements? "))
+    else:
+      size = int(args[1])
 
-array = randArray(size)
-print(array)
+    array = randArray(size, 0, 100)
+    print(array)
