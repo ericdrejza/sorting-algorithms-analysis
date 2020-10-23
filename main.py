e0925a825ff886bb6f3b5c5e5d8c29744fb908d0
Eric Drejza <edrejza@udel.edu>
2020-10-23 01:24:06 -0400

    Added insertion sort to each of section in main.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import argparse
 from datetime import datetime
 from array_producer import randArray
 from bubblesort import bubblesort
+from insertion_sort import insertion_sort
 from selection_sort import selection_sort
 
 #************** ARGUMENTS ******************
@@ -11,17 +12,16 @@ parser.add_argument('-a', '--all', dest='all_sorts', action='store_const',
                     const=True, default=False)
 parser.add_argument('--avg', '--averages', dest='averages', action='store_const',
                     const=True, default=False)
-parser.add_argument('-s', '--sorted', dest='sorted', action='store_const',
+parser.add_argument('-s', '--sorted', dest='sorted_sorts', action='store_const',
                     const=True, default=False)
 parser.add_argument('-t', '--tests', dest='tests', action='store_const',
                     const=True, default=False)
 
-args = sys.argv
-# print(args)
+args = parser.parse_args()
+print(args)
 
-if len(args) == 1:
-    all_sorts = True
-    
+if len(sys.argv) == 1:
+    args.all_sorts = True
 
 #************** FUNCTIONS ******************
 def sort_and_time(array, sort_func, will_print):
@@ -29,8 +29,7 @@ def sort_and_time(array, sort_func, will_print):
     sort_func(array)
     elapsed = datetime.now() - now
     if will_print:
-        print(array)
-        print(str(sort_func).split()[1] + "(Runtime in Milliseconds): " + str(elapsed.microseconds/1000) + " ms\n")
+        print(str(sort_func).split()[1] + "(Runtime in Milliseconds): " + str(elapsed.microseconds/1000) + " ms")
     return elapsed
 
 def avg_sort_and_time(sort_func, reps, num_elements, lowerBound, upperBound):
@@ -40,126 +39,142 @@ def avg_sort_and_time(sort_func, reps, num_elements, lowerBound, upperBound):
         average += sort_and_time(a, sort_func, False)
     average = average / reps
     # print(str(sort_func).split()[1] + "(Average): " + str(average)[2:])
-    print(str(sort_func).split()[1] + "(Average Runtime in Milliseconds): " + str(average.microseconds/1000) + "ms\n")
+    print(str(sort_func).split()[1] + "(Average Runtime in Milliseconds): " + str(average.microseconds/1000) + "ms")
 
 
 
 #************** AVERAGES OF SORT CALLS ******************
-# Find the averages of the various sorting algorithms
-print("Average Runtime of Each Sorting Algorithm:\n")
+if args.all_sorts or args.averages:
+    # Find the averages of the various sorting algorithms
+    print("\n".ljust(80, '-'))
+    print("Average Runtime of Each Sorting Algorithm:\n")
 
-# Variables
-REPS = 10
-NUM_ELEMENTS = 1000
-LOWER_BOUND = 0
-UPPER_BOUND = NUM_ELEMENTS * 10 - 1
+    # Variables
+    REPS = 10
+    NUM_ELEMENTS = 1000
+    LOWER_BOUND = 0
+    UPPER_BOUND = NUM_ELEMENTS * 10 - 1
 
-# Print values of the variables
-print("Repitions per algorithm: " + str(REPS))
-print("Number of elements in array: " + str(NUM_ELEMENTS))
-print("Lower Bound: " + str(LOWER_BOUND) + "; Upper Bound: " + str(UPPER_BOUND) + "\n")
+    # Print values of the variables
+    print("Repitions per algorithm: " + str(REPS))
+    print("Number of elements in array: " + str(NUM_ELEMENTS))
+    print("Lower Bound: " + str(LOWER_BOUND) + "; Upper Bound: " + str(UPPER_BOUND) + "\n")
 
-# Bubblesort
-avg_sort_and_time(bubblesort, REPS, NUM_ELEMENTS, LOWER_BOUND, UPPER_BOUND)
+    # Bubblesort
+    avg_sort_and_time(bubblesort, REPS, NUM_ELEMENTS, LOWER_BOUND, UPPER_BOUND)
 
-# Insertion sort
-# TODO: Add Insertion sort call
+    # Insertion sort
+    avg_sort_and_time(insertion_sort, REPS, NUM_ELEMENTS, LOWER_BOUND, UPPER_BOUND)
 
-# Selection Sort
-avg_sort_and_time(selection_sort, REPS, NUM_ELEMENTS, LOWER_BOUND, UPPER_BOUND)
+    # Selection Sort
+    avg_sort_and_time(selection_sort, REPS, NUM_ELEMENTS, LOWER_BOUND, UPPER_BOUND)
 
-# Mergesort
-# TODO: Add Mergesort call
+    # Mergesort
+    # TODO: Add Mergesort call
 
-# Modified Mergesort (Insertion at array size <= 8)
-# TODO: Add Modified Mergesort call
+    # Modified Mergesort (Insertion at array size <= 8)
+    # TODO: Add Modified Mergesort call
 
-# Heapsort
-# TODO: Add Heapsort call
+    # Heapsort
+    # TODO: Add Heapsort call
 
-# BST Sort
-# TODO: Add BST sort call
+    # BST Sort
+    # TODO: Add BST sort call
 
-# LLRB Sort
-# TODO: Add LLRB sort call
+    # LLRB Sort
+    # TODO: Add LLRB sort call
 
 
 #************** SORTED ARRAY SORT CALLS ******************
-print("\n".ljust(80, '-'))
-print("Runtime of Each Sorting Algorithm Using Sorted Arrays:\n")
+if args.all_sorts or args.sorted_sorts:
+    print("\n".ljust(80, '-'))
+    print("Runtime of Each Sorting Algorithm Using Sorted Arrays:\n")
 
-# Variables
-NUM_ELEMENTS_SORTED = 10
-LOWER_BOUND_SORTED = 0
-UPPER_BOUND_SORTED = NUM_ELEMENTS_SORTED * 10 - 1
+    # Variables
+    NUM_ELEMENTS_SORTED = 500
+    LOWER_BOUND_SORTED = 0
+    UPPER_BOUND_SORTED = NUM_ELEMENTS_SORTED * 10 - 1
+    PRINT_SORTED = True
 
-# Print values of the variables
-print("Number of elements in array: " + str(NUM_ELEMENTS_SORTED))
-print("Lower Bound: " + str(LOWER_BOUND_SORTED) + "; Upper Bound: " + str(UPPER_BOUND_SORTED) + "\n")
+    # Print values of the variables
+    print("Number of elements in array: " + str(NUM_ELEMENTS_SORTED))
+    print("Lower Bound: " + str(LOWER_BOUND_SORTED) + "; Upper Bound: " + str(UPPER_BOUND_SORTED) + "\n")
 
-a = randArray(NUM_ELEMENTS_SORTED, LOWER_BOUND_SORTED, UPPER_BOUND_SORTED)
-selection_sort(a)
+    a = list(range(0, NUM_ELEMENTS_SORTED))
 
-# Bubblesort
-sort_and_time(a, bubblesort, True)
+    # Bubblesort
+    sort_and_time(a, bubblesort, PRINT_SORTED)
 
-# Insertion sort
-# TODO: Add Insertion sort call
+    # Insertion sort
+    sort_and_time(a, insertion_sort, PRINT_SORTED)
 
-# Selection Sort
-sort_and_time(a, selection_sort, True)
+    # Selection Sort
+    sort_and_time(a, selection_sort, PRINT_SORTED)
 
-# Mergesort
-# TODO: Add Mergesort call
+    # Mergesort
+    # TODO: Add Mergesort call
 
-# Modified Mergesort (Insertion at array size <= 8)
-# TODO: Add Modified Mergesort call
+    # Modified Mergesort (Insertion at array size <= 8)
+    # TODO: Add Modified Mergesort call
 
-# Heapsort
-# TODO: Add Heapsort call
+    # Heapsort
+    # TODO: Add Heapsort call
 
-# BST Sort
-# TODO: Add BST sort call
+    # BST Sort
+    # TODO: Add BST sort call
 
-# LLRB Sort
-# TODO: Add LLRB sort call
+    # LLRB Sort
+    # TODO: Add LLRB sort call
 
 
 
 #*********** TESTS **************
-print("\n".ljust(80, '-'))
-print("Runtime of Each Sorting Algorithm Using Sorted Arrays:\n")
+if args.all_sorts or args.tests:
+    print("\n".ljust(80, '-'))
+    print("Runtime of each sort algorithm on a random array:\n")
+    
+    # Variables
+    NUM_ELEMENTS_TEST = 10
+    LOWER_BOUND_TEST = 0
+    UPPER_BOUND_TEST = NUM_ELEMENTS_TEST * 10 - 1
+    PRINT_TEST = True
+    
+    # Print values of the variables
+    print("Number of elements in array: " + str(NUM_ELEMENTS_TEST))
+    print("Lower Bound: " + str(LOWER_BOUND_TEST) + "; Upper Bound: " + str(UPPER_BOUND_TEST) + "\n")
+    
 
-# Variables
-NUM_ELEMENTS_TEST = 10
-LOWER_BOUND_TEST = 0
-UPPER_BOUND_TEST = NUM_ELEMENTS_TEST * 10 - 1
-
-# Print values of the variables
-print("Number of elements in array: " + str(NUM_ELEMENTS_TEST))
-print("Lower Bound: " + str(LOWER_BOUND_TEST) + "; Upper Bound: " + str(UPPER_BOUND_TEST) + "\n")
-
-a = randArray(NUM_ELEMENTS_TEST, LOWER_BOUND_TEST, UPPER_BOUND_TEST)
-# Bubblesort
-sort_and_time(a, bubblesort, True)
-
-# Insertion sort
-# TODO: Add Insertion sort call
-
-# Selection Sort
-sort_and_time(a, selection_sort, True)
-
-# Mergesort
-# TODO: Add Mergesort call
-
-# Modified Mergesort (Insertion at array size <= 8)
-# TODO: Add Modified Mergesort call
-
-# Heapsort
-# TODO: Add Heapsort call
-
-# BST Sort
-# TODO: Add BST sort call
-
-# LLRB Sort
-# TODO: Add LLRB sort call
+    # Bubblesort
+    a = randArray(NUM_ELEMENTS_TEST, LOWER_BOUND_TEST, UPPER_BOUND_TEST)
+    sort_and_time(a, bubblesort, PRINT_TEST)
+    print(a)
+    
+    # Insertion sort
+    a = randArray(NUM_ELEMENTS_TEST, LOWER_BOUND_TEST, UPPER_BOUND_TEST)
+    sort_and_time(a, insertion_sort, PRINT_TEST)
+    print(a)
+    
+    # Selection Sort
+    a = randArray(NUM_ELEMENTS_TEST, LOWER_BOUND_TEST, UPPER_BOUND_TEST)
+    sort_and_time(a, selection_sort, PRINT_TEST)
+    print(a)
+    
+    # Mergesort
+    a = randArray(NUM_ELEMENTS_TEST, LOWER_BOUND_TEST, UPPER_BOUND_TEST)
+    # TODO: Add Mergesort call
+    
+    # Modified Mergesort (Insertion at array size <= 8)
+    a = randArray(NUM_ELEMENTS_TEST, LOWER_BOUND_TEST, UPPER_BOUND_TEST)
+    # TODO: Add Modified Mergesort call
+    
+    # Heapsort
+    a = randArray(NUM_ELEMENTS_TEST, LOWER_BOUND_TEST, UPPER_BOUND_TEST)
+    # TODO: Add Heapsort call
+    
+    # BST Sort
+    a = randArray(NUM_ELEMENTS_TEST, LOWER_BOUND_TEST, UPPER_BOUND_TEST)
+    # TODO: Add BST sort call
+    
+    # LLRB Sort
+    a = randArray(NUM_ELEMENTS_TEST, LOWER_BOUND_TEST, UPPER_BOUND_TEST)
+    # TODO: Add LLRB sort call
