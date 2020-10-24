20b01cb5547b5158c20f1e635a44fd8b31578c4f
Eric Drejza <edrejza@udel.edu>
2020-10-24 12:54:36 -0400

    Implemented mergesort_insertion.

--- mergesort.py
+++ mergesort.py
@@ -1,4 +1,6 @@
-# Python Mergesort Implementation - Structure from Lecture Slide 5b
+# Python Mergesort Implementation - Structure from Lecture Slides 5b
+
+from insertion_sort import insertion_sort
 
 def mergesort(array):
     n = len(array)
@@ -14,6 +16,22 @@ def mergesort(array):
         # merge
         merge(array, left, right)
 
+
+def mergesort_insertion(array):
+    n = len(array)
+    if n > 8:
+        mid = n // 2
+        left = array[:mid]
+        right = array[mid:]
+
+        # mergesort on both halves
+        mergesort(left)
+        mergesort(right)
+
+        # merge
+        merge(array, left, right)
+    else:
+        insertionSort(array)
 
 
 def merge(array, left, right):
